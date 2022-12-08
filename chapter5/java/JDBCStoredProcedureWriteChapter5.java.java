c45a8415914a0447d4485ce0798266f7b4e38801
Stephen Morris <stephen.morris112@gmail.com>
2022-12-08 15:45:27 +0000

    Update and rename JDBCStoredProcedureWrite.java to JDBCStoredProcedureWriteChapter5.java.java

---
+++ chapter5/java/JDBCStoredProcedureWriteChapter5.java.java
@@ -0,0 +1,34 @@
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class JDBCStoredProcedureWriteChapter5 {
+
+    public static void main(String[] args) {
+            Connection connection = null;
+            CallableStatement statement = null;
+            String action_message = "Hello from Java";
+            String code_location = "In Java";
+            try {
+                connection = DBConnection.getConnection();
+                statement = connection.prepareCall("{call LOGGINGPACKAGE.LOG_ACTION(?, ?)}");
+                statement.setString("action_message", action_message);
+                statement.setString("code_location", code_location);
+                statement.executeUpdate();
+                System.out.println("Successful call");
+            } catch(Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (!statement.isClosed()) {
+                        statement.close();
+                    }
+                    if (!connection.isClosed()) {
+                        connection.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+}
